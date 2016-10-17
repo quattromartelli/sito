30c0fa418bfa1905755b218a12f2e2c5f2b3c965
NXNZ <annunziatomazzaferro@AirdiAnnunziato.station>
2016-10-17 02:02:47 +0200

    funzioni jquery per il giochino
    l’ho messo in italiano e accorciato.
    se si vuole allungare basta continuare con lo stesso pattern. l’unico
    problema da risolvere sta nella schermata che si alza e costringe a
    scrollare dinuovo in basso. penso sia facilmente risolvibile. il
    giochino ha 3 fasi, poi appare il footer. nelle prime due si da la
    possibilità di poter reversare il gioco (

--- script/script.js
+++ script/script.js
@@ -28,6 +28,34 @@ $(document).ready(function() {
     $('img').duotone({
         gradientMap: '#11192D, #FF1453 50%'
     });
+    // inizio footergame, nascondo tutti gli altri messaggi e il footer scherzo
+    $('#fase2').hide();
+    $('#fase3').hide();
+    $('#footer').hide();
+    // se si clicca vabene il messaggio va via
+    $('#vabene').on("click", function(){
+      $('#fase1').fadeOut();
+    });
+    // se si clicca l'altro appare il secondo messaggio, il primo sparisce
+    $('#idontcare').on("click", function(){
+      $('#fase1').fadeOut();
+      $('#fase2').delay(800).fadeIn();
+    });
+    // vabene dinuovo, sparisce e basta
+    $('#vabene2').on("click", function(){
+      $('#fase2').fadeOut();
+    });
+    // se si clicca l'altra opzione appare il terzo messaggio, sparisce il secondo
+    $('#idontcare2').on("click", function(){
+      $('#fase2').fadeOut();
+      $('#fase3').delay(800).fadeIn();
+    });
+    // ultimo messaggio, poi appare il footer 
+    $('#idontcare3').on("click", function(){
+      $('#idontcare3').parent().fadeOut();
+      $('#footer').delay(800).fadeIn();
+    });
+
 });
 
 $("body").on("contextmenu", "img", function(e) {
@@ -37,3 +65,4 @@ $("body").on("contextmenu", "img", function(e) {
 $("body").on("click", "img", function(e) { return false; });
 
 $('img').on('dragstart', function(event) { event.preventDefault(); });
+
