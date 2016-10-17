f9ce4e136ecd656a829e927e6ddae188aff6de31
drivinward <edoardo.guido.93@gmail.com>
2016-10-17 10:51:52 +0200

    Aggiunta script + riordine codice
    - Aggiunto script footergame.js
    - Aggiunta classe CSS per scelte del footergame
    - Migliorate le transizioni del footergame
    - Rimossi link ridondanti a font inutilizzate

--- script/script.js
+++ script/script.js
@@ -28,34 +28,6 @@ $(document).ready(function() {
     $('img').duotone({
         gradientMap: '#11192D, #FF1453 50%'
     });
-    // inizio footergame, nascondo tutti gli altri messaggi e il footer scherzo
-    $('#fase2').hide();
-    $('#fase3').hide();
-    $('#footer').hide();
-    // se si clicca vabene il messaggio va via
-    $('#vabene').on("click", function(){
-      $('#fase1').fadeOut();
-    });
-    // se si clicca l'altro appare il secondo messaggio, il primo sparisce
-    $('#idontcare').on("click", function(){
-      $('#fase1').fadeOut();
-      $('#fase2').delay(800).fadeIn();
-    });
-    // vabene dinuovo, sparisce e basta
-    $('#vabene2').on("click", function(){
-      $('#fase2').fadeOut();
-    });
-    // se si clicca l'altra opzione appare il terzo messaggio, sparisce il secondo
-    $('#idontcare2').on("click", function(){
-      $('#fase2').fadeOut();
-      $('#fase3').delay(800).fadeIn();
-    });
-    // ultimo messaggio, poi appare il footer 
-    $('#idontcare3').on("click", function(){
-      $('#idontcare3').parent().fadeOut();
-      $('#footer').delay(800).fadeIn();
-    });
-
 });
 
 $("body").on("contextmenu", "img", function(e) {
