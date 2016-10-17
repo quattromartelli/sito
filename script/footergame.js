eb88541a920c412253363b3d83e593678b25663f
drivinward <edoardo.guido.93@gmail.com>
2016-10-17 11:09:42 +0200

    Miglioramento transizioni e layout + riordine codice
    - Migliorato layout durante l’apparizione del footer
    - Aggiornato margine footer
    - Aggiunta classe CSS .footergamecontainer per nascondere tutto lo
    spazio una volta finito il gioco
    - Rimosso il file .gitignore che impediva la sincronizzazione dello
    script footergame

--- script/footergame.js
+++ script/footergame.js
@@ -4,31 +4,37 @@ $(document).ready(function() {
     $('#fase2').hide();
     $('#fase3').hide();
     $('#footer').hide();
+    
     // se si clicca vabene il messaggio va via
     $('#vabene').on("click", function(){
       $('#fase1').fadeOut(800);
     });
+    
     // se si clicca l'altro appare il secondo messaggio, il primo sparisce
     $('#idontcare').on("click", function(){
       $('#fase1').fadeOut(200);
       $('#fase2').delay(800).fadeIn(200);
     });
+    
     // vabene dinuovo, sparisce e basta
     $('#vabene2').on("click", function(){
       $('#fase2').fadeOut(800);
     });
+    
     // se si clicca l'altra opzione appare il terzo messaggio, sparisce il secondo
     $('#idontcare2').on("click", function(){
       $('#fase2').fadeOut(200);
       $('#fase3').delay(800).fadeIn(200);
     });
+    
     // ultimo messaggio, poi appare il footer 
     $('#idontcare3').on("click", function(){
-      $('#idontcare3').parent().fadeOut(200);
-      $('#footer').delay(800).fadeIn(200);
+      $('#footergamecontainer').fadeOut(200);
+      $('#footer').delay(0).fadeIn(200);
       $("html, body").animate({ 
           scrollTop: $(document).height() 
       }, 800);
+        
     });
     // fine footergame
 
