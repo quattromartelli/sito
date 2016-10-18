f09a4e61d736dc4f5c444845a5b6b553b3563250
drivinward <edoardo.guido.93@gmail.com>
2016-10-18 13:58:45 +0200

    Traduzione testi + Variabili CSS + miglioramento funzione footergame

--- script/footergame.js
+++ script/footergame.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    var fadeSpeed = 200;
+    var delayAmount = 500;
 
     // inizio footergame, nascondo tutti gli altri messaggi e il footer scherzo
     $('#fase2').hide();
@@ -7,33 +9,31 @@ $(document).ready(function() {
     
     // se si clicca vabene il messaggio va via
     $('#vabene').on("click", function(){
-      $('#fase1').fadeOut(800);
+      $('#fase1').fadeOut(delayAmount);
     });
     
     // se si clicca l'altro appare il secondo messaggio, il primo sparisce
     $('#idontcare').on("click", function(){
-      $('#fase1').fadeOut(200);
-      $('#fase2').delay(800).fadeIn(200);
+      $('#fase1').fadeOut(fadeSpeed);
+      $('#fase2').delay(delayAmount).fadeIn(fadeSpeed);
     });
     
     // vabene dinuovo, sparisce e basta
     $('#vabene2').on("click", function(){
-      $('#fase2').fadeOut(800);
+      $('#fase2').fadeOut(delayAmount);
     });
     
     // se si clicca l'altra opzione appare il terzo messaggio, sparisce il secondo
     $('#idontcare2').on("click", function(){
-      $('#fase2').fadeOut(200);
-      $('#fase3').delay(800).fadeIn(200);
+      $('#fase2').fadeOut(fadeSpeed);
+      $('#fase3').delay(delayAmount).fadeIn(fadeSpeed);
     });
     
     // ultimo messaggio, poi appare il footer 
     $('#idontcare3').on("click", function(){
-      $('#footergamecontainer').fadeOut(200);
-      $('#footer').delay(0).fadeIn(200);
-//      $("html, body").animate({ 
-//          scrollTop: $(document).height() 
-//      }, 800);
+      $('#footergamecontainer').fadeOut(fadeSpeed);
+      $('#footer').delay(0).fadeIn(1000);
+      $("html, body").animate({ scrollTop: $(footer).offset().bottom }, delayAmount);
         
     });
     // fine footergame
